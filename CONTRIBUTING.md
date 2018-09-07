11929067c289559fccad6d4709e55afd6fbc48cc
Francesco Schwarz <francesco.schwarz@posteo.de>
2018-09-07 23:55:04 +0200

    Update dependencies and README

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -4,18 +4,16 @@ Pull requests go into the `master` branch. The `gh-pages` branch is a presentati
 
 ## Prerequisites
 
-- You must have [Node.js](https://nodejs.org/) installed to run the *Specificity Visualizer* locally.
+- You must have [**Node.js**](https://nodejs.org/) and [**Yarn**](https://yarnpkg.com/lang/en/) installed to run the *Specificity Visualizer* locally.
 - Clone the repository with
 `git clone https://github.com/isellsoap/specificity-visualizer.git`
 - Go into the folder with
 `cd specificity-visualizer`
 
+## Developing
+
+- **`yarn start`** starts the application in development mode and continuously watches all files.
+
 ## Building
 
-- Install the dependencies with
-`npm install`
-- Build all the assets once with
-`npm run build`
-or watch the files continuously with
-`npm run watch`
-- This will create the `index.html` in the `dist` folder which you can now open in your browser.
+- **`yarn build`** builds the application in production mode.
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,28 +1,19 @@
-import * as webpack from 'webpack';
-
 // node packages
 import * as path from 'path';
 
 // webpack plugins
 import * as CleanPlugin from 'clean-webpack-plugin';
-import * as ExtractTextPlugin from 'extract-text-webpack-plugin';
 import * as HtmlPlugin from 'html-webpack-plugin';
+import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import * as StyleLintPlugin from 'stylelint-webpack-plugin';
 
 // postcss plugins
 import * as postcssAutoprefixer from 'autoprefixer';
 
-interface Config extends webpack.Configuration {
-  module?: {
-    rules: webpack.NewUseRule[]
-  },
-  resolve?: webpack.NewResolve
-}
-
-module.exports = (env) => {
-  const isProductionBuild = env.production === 'true';
+module.exports = () => {
 
   return {
+    mode: process.env.NODE_ENV,
     entry: {
       'scripts/main': './src/scripts/main.js',
       'scripts/webfontloader': './src/scripts/fontLoader.js'
@@ -32,7 +23,7 @@ module.exports = (env) => {
       filename: '[name].[chunkhash].js'
     },
     module: {
-      loaders: [
+      rules: [
         // scripts
         {
           enforce: 'pre',
@@ -48,27 +39,25 @@ module.exports = (env) => {
         // styles
         {
           test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader',
-              {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: [
-                    postcssAutoprefixer
-                  ]
-                }
-              },
-              'sass-loader'
-            ]
-          })
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  postcssAutoprefixer
+                ]
+              }
+            },
+            'sass-loader'
+          ]
         }
       ]
     },
     plugins: [
       new CleanPlugin(['./dist']),
-      new ExtractTextPlugin({
+      new MiniCssExtractPlugin({
         filename: 'styles/main.[contenthash].css'
       }),
       new StyleLintPlugin({
@@ -80,7 +69,7 @@ module.exports = (env) => {
         template: './src/markup/index.html.ejs',
         filename: 'index.html',
         minify: {
-          minifyJS: isProductionBuild
+          minifyJS: process.env.NODE_ENV === 'production'
         },
         favicon: 'src/images/favicon.ico'
       })
